b3c91b1761ed7252dbef36f1e7974c93768cf126
lindellcarternyc <lindellcarternyc@gmail.com>
2022-08-07 00:17:22 -0400

    adds buttons to sidebar

--- src/components/Sidebar/Sidebar.svelte
+++ src/components/Sidebar/Sidebar.svelte
@@ -19,13 +19,21 @@ const user = {
   <Search searchTerm={searchTerm} handleInput={handleInput}/>
   <GroupList />
   <div class="buttons">
-    Buttons
+    <button class="new-list">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+      </svg>
+      Add New List</button>
+    <button class="new-group">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+      </svg>
+    </button>
   </div>
 </div>
 
 <style>
 .sidebar {
-  border: 1px solid red;
   width: 300px;
   display: none;
   height: 100vh;
@@ -37,10 +45,41 @@ const user = {
 
 .buttons {
   position: absolute;
-  border: 1px solid green;
+  background-color: lightgray;
+  padding: 0.75rem;
   width: 100%;
   bottom: 0;
   left: 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+button {
+  border-radius: 0.25rem;
+  padding: 0.5rem 0.75rem;
+  display: flex;
+  outline: none;
+  border: none;
+  background-color: transparent;
+}
+
+button:hover {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+.new-list {
+  display: flex;
+}
+
+.new-list svg {
+  width: 24px;
+  height: 24px;
+  margin-right: 0.5rem;
+}
+
+.new-group svg {
+  width: 24px;
+  height: 24px;
 }
 
 @media (min-width: 900px) {
